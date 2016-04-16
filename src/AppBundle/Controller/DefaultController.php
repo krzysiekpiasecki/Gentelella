6dfaef7ef837740c870c838f9ebc702594fb6da9
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-04-16 09:20:32 +0200

    Preview and site

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,15 +9,24 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("gentellela/{name}", name="homepage")
+     * @Route("preview/{name}", name="preview", defaults={"name": "index"})
+     */
+    public function previewAction(Request $request, $name)
+    {
+        // replace this example code with whatever you need
+        return $this->render(sprintf('preview/%s.html.twig', $name));
+    }
+
+    /**
+     * @Route("{name}", name="homepage", defaults={"name": "index"})
      */
     public function indexAction(Request $request, $name)
     {
+        $realPath = sprintf('%s/../', realpath($this->getParameter('kernel.root_dir')));
+        if (! is_readable(sprintf('%s/%s.html.twig', $realPath, $name))) {
+            $name = "index";
+        }
         // replace this example code with whatever you need
-        return $this->render(
-            sprintf('default/%s.html.twig', $name), [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render(sprintf('%s.html.twig', $name));
     }
-
 }
