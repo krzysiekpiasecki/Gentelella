1fba6c7efdf202f36742337a2af1bb470e3a46c5
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-04-17 12:24:42 +0200

    Prefix with admin

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,17 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
+     * Template homepage
+     *
      * @Route("template/{name}", name="template", defaults={"name": "index"})
+     * @param Request $request
+     * @param string $name
+     * @return Response
      */
     public function previewAction(Request $request, $name)
     {
@@ -17,14 +23,30 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("{name}", name="homepage", defaults={"name": "index"})
+     * Admin homepage
+     *
+     * @Route("admin/{name}", name="admin_homepage", defaults={"name": "index"})
+     * @param Request $request
+     * @param string $name
+     * @return Response
      */
     public function indexAction(Request $request, $name)
     {
         if (!$this->get('templating')->exists(sprintf('%s.html.twig', $name))) {
             $name = 'index';
         }
-
         return $this->render(sprintf('%s.html.twig', $name));
+    }
+
+    /**
+     * Redirect to admin homepage
+     *
+     * @Route("/", name="homepage")
+     */
+    public function redirectAction(Request $request)
+    {
+        return $this->redirectToRoute('admin_homepage', [
+            'name' => 'index'
+        ]);
     }
 }
