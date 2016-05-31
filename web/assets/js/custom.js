5990b3cde0313413653f4e4522aac7e42f748dc0
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-05-31 22:39:27 +0200

    Update gentelella template

--- web/assets/js/custom.js
+++ web/assets/js/custom.js
@@ -21,12 +21,13 @@ $(document).ready(function() {
         // reset height
         $RIGHT_COL.css('min-height', $(window).height());
 
-        var bodyHeight = $BODY.height(),
+        var bodyHeight = $BODY.outerHeight(),
+            footerHeight = $BODY.hasClass('footer_fixed') ? 0 : $FOOTER.height(),
             leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
             contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
         // normalize content
-        contentHeight -= $NAV_MENU.height() + $FOOTER.height();
+        contentHeight -= $NAV_MENU.height() + footerHeight;
 
         $RIGHT_COL.css('min-height', contentHeight);
     };
@@ -35,14 +36,14 @@ $(document).ready(function() {
         var $li = $(this).parent();
 
         if ($li.is('.active')) {
-            $li.removeClass('active');
+            $li.removeClass('active active-sm');
             $('ul:first', $li).slideUp(function() {
                 setContentHeight();
             });
         } else {
             // prevent closing menu if we are on child menu
             if (!$li.parent().is('.child_menu')) {
-                $SIDEBAR_MENU.find('li').removeClass('active');
+                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
                 $SIDEBAR_MENU.find('li ul').slideUp();
             }
             
@@ -57,18 +58,14 @@ $(document).ready(function() {
     // toggle small or large menu
     $MENU_TOGGLE.on('click', function() {
         if ($BODY.hasClass('nav-md')) {
-            $BODY.removeClass('nav-md').addClass('nav-sm');
-
-            if ($SIDEBAR_MENU.find('li').hasClass('active')) {
-                $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
-            }
+            $SIDEBAR_MENU.find('li.active ul').hide();
+            $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
         } else {
-            $BODY.removeClass('nav-sm').addClass('nav-md');
-
-            if ($SIDEBAR_MENU.find('li').hasClass('active-sm')) {
-                $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
-            }
+            $SIDEBAR_MENU.find('li.active-sm ul').show();
+            $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
         }
+
+        $BODY.toggleClass('nav-md nav-sm');
 
         setContentHeight();
     });
@@ -86,6 +83,8 @@ $(document).ready(function() {
     $(window).smartresize(function(){  
         setContentHeight();
     });
+
+    setContentHeight();
 
     // fixed sidebar
     if ($.fn.mCustomScrollbar) {
@@ -136,7 +135,7 @@ $(document).ready(function() {
 
 // Progressbar
 if ($(".progress .progress-bar")[0]) {
-    $('.progress .progress-bar').progressbar(); // bootstrap 3
+    $('.progress .progress-bar').progressbar();
 }
 // /Progressbar
 
@@ -243,7 +242,6 @@ if (typeof NProgress != 'undefined') {
         NProgress.done();
     });
 }
-
 /**
  * Resize function without multiple trigger
  * 
