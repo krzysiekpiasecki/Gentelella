d057e89821b139ca530314b39c605b9593c5898d
MedUnes <contact@medunes.net>
2017-06-12 19:01:38 +0100

    fixed: php-cs

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -8,9 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * 
+ *
  * File overridden  by Medunes (mohamed.younes@yandex.com)
- * 
+ *
  */
 
 namespace AppBundle\Controller;
