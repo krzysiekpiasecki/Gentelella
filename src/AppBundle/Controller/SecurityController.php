c40a2ce33057779d1755683697378bbe58eb497c
MedUnes <contact@medunes.net>
2017-06-12 20:25:25 +0100

    fixed php-cs

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,6 +1,6 @@
 <?php
 
-	/*
+/*
  * This file is part of the FOSUserBundle package.
  *
  * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
@@ -13,9 +13,8 @@
  *
  */
 
-	namespace AppBundle\Controller;
+namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -24,50 +23,49 @@ use FOS\UserBundle\Controller\SecurityController as BaseController;
 
 class SecurityController extends BaseController
 {
-	/**
+    /**
      * @param Request $request
      *
      * @return Response
      */
-	public function loginAction(Request $request)
-	{
-		/** @var $session \Symfony\Component\HttpFoundation\Session\Session */
-		$session = $request->getSession();
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
 
-		$authErrorKey = Security::AUTHENTICATION_ERROR;
-		$lastUsernameKey = Security::LAST_USERNAME;
+        $authErrorKey = Security::AUTHENTICATION_ERROR;
+        $lastUsernameKey = Security::LAST_USERNAME;
 
-		// get the error if any (works with forward and redirect -- see below)
-		if ($request->attributes->has($authErrorKey)) {
-			$error = $request->attributes->get($authErrorKey);
-		} elseif (null !== $session && $session->has($authErrorKey)) {
-			$error = $session->get($authErrorKey);
-			$session->remove($authErrorKey);
-		} else {
-			$error = null;
-		}
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
 
-		if (!$error instanceof AuthenticationException) {
-			$error = null; // The value does not come from the security component.
-		}
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
 
-		// last username entered by the user
-		$lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
 
-		$csrfToken = $this->has('security.csrf.token_manager')
-			? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
-			: null;
+        $csrfToken = $this->has('security.csrf.token_manager')
+            ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+            : null;
 
+        // Build an instance of the registration form and send it to the mixed login-register view template
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        $form = $formFactory->createForm();
 
-		// Build an instance of the registration form and send it to the mixed login-register view template
-		$formFactory=$this->get("fos_user.registration.form.factory");
-		$form=$formFactory->createForm();
-
-		return $this->renderLogin(array(
-			'last_username' => $lastUsername,
-			'error' => $error,
-			'csrf_token' => $csrfToken,
-			'registration_form'=>$form->createView(), //this is what we shall add
-		));
-	}
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken,
+            'registration_form' => $form->createView(), //this is what we shall add
+        ));
+    }
 }
