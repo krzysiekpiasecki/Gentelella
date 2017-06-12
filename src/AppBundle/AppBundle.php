77849c24d5eeea807e63fcbd37d5de013cda76f5
MedUnes <contact@medunes.net>
2017-06-12 18:30:36 +0100

    fixed: php-cs

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -6,9 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-	public function getParent()
+    public function getParent()
     {
         return 'FOSUserBundle';
     }
 }
-
