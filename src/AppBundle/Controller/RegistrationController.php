e985480caa44160c5586b6580d835e1b5a1087b7
MedUnes <contact@medunes.net>
2017-06-12 19:25:41 +0100

    Update RegistrationController.php

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+	/*
  * This file is part of the FOSUserBundle package.
  *
  * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
@@ -11,7 +11,7 @@
  * File overridden  by Medunes (mohamed.younes@yandex.com)
  */
 
-namespace AppBundle\Controller;
+	namespace AppBundle\Controller;
 
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use FOS\UserBundle\Event\FormEvent;
@@ -41,89 +41,89 @@ use FOS\UserBundle\Controller\RegistrationController as BaseController;
  */
 class RegistrationController extends BaseController
 {
-    /**
+	/**
      * @param Request $request
      *
      * @return Response
      */
-    public function registerAction(Request $request)
-    {
-        /** @var $formFactory FactoryInterface */
-        $formFactory = $this->get('fos_user.registration.form.factory');
-        /** @var $userManager UserManagerInterface */
-        $userManager = $this->get('fos_user.user_manager');
-        /** @var $dispatcher EventDispatcherInterface */
-        $dispatcher = $this->get('event_dispatcher');
+	public function registerAction(Request $request)
+	{
+		/** @var $formFactory FactoryInterface */
+		$formFactory = $this->get('fos_user.registration.form.factory');
+		/** @var $userManager UserManagerInterface */
+		$userManager = $this->get('fos_user.user_manager');
+		/** @var $dispatcher EventDispatcherInterface */
+		$dispatcher = $this->get('event_dispatcher');
 
-        $user = $userManager->createUser();
-        $user->setEnabled(true)
-             ->addRole("ROLE_ADMIN"); // Since the firewall is configured to restrict access to ADMIN_ROLE users
+		$user = $userManager->createUser();
+		$user->setEnabled(true)
+			->addRole("ROLE_ADMIN"); // Since the firewall is configured to restrict access to ADMIN_ROLE users
 
-        $event = new GetResponseUserEvent($user, $request);
-        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+		$event = new GetResponseUserEvent($user, $request);
+		$dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
 
-        if (null !== $event->getResponse()) {
-            return $event->getResponse();
-        }
+		if (null !== $event->getResponse()) {
+			return $event->getResponse();
+		}
 
-        $form = $formFactory->createForm();
-        $form->setData($user);
+		$form = $formFactory->createForm();
+		$form->setData($user);
 
-        $form->handleRequest($request);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            if ($form->isValid()) {
-                $event = new FormEvent($form, $request);
-                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+		if ($form->isSubmitted()) {
+			if ($form->isValid()) {
+				$event = new FormEvent($form, $request);
+				$dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
 
-                $userManager->updateUser($user);
+				$userManager->updateUser($user);
 
-                if (null === $response = $event->getResponse()) {
-                    $url = $this->generateUrl('fos_user_registration_confirmed');
-                    $response = new RedirectResponse($url);
-                }
+				if (null === $response = $event->getResponse()) {
+					$url = $this->generateUrl('fos_user_registration_confirmed');
+					$response = new RedirectResponse($url);
+				}
 
-                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+				$dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
 
-                return $response;
-            }
+				return $response;
+			}
 
-            $event = new FormEvent($form, $request);
-            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
+			$event = new FormEvent($form, $request);
+			$dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
 
 
-            if (null !== $response = $event->getResponse()) {
-                return $this->render('@FOSUser/Security/login.html.twig', $this->buildTemplateData($form->createView(), $request, '#signup'));
-            }
-        }
+			if (null !== $response = $event->getResponse()) {
+				return $this->render('@FOSUser/Security/login.html.twig', $this->buildTemplateData($form->createView(), $request, '#signup'));
+			}
+		}
 
-        return $this->render('@FOSUser/Security/login.html.twig', $this->buildTemplateData($form->createView(), $request, '#signup'));
-    }
+		return $this->render('@FOSUser/Security/login.html.twig', $this->buildTemplateData($form->createView(), $request, '#signup'));
+	}
 
-    /**
+	/**
      * Tell the user to check their email provider.
      */
-    public function checkEmailAction()
-    {
-        $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
+	public function checkEmailAction()
+	{
+		$email = $this->get('session')->get('fos_user_send_confirmation_email/email');
 
-        if (empty($email)) {
-            return new RedirectResponse($this->get('router')->generate('fos_user_registration_register'));
-        }
+		if (empty($email)) {
+			return new RedirectResponse($this->get('router')->generate('fos_user_registration_register'));
+		}
 
-        $this->get('session')->remove('fos_user_send_confirmation_email/email');
-        $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
+		$this->get('session')->remove('fos_user_send_confirmation_email/email');
+		$user = $this->get('fos_user.user_manager')->findUserByEmail($email);
 
-        if (null === $user) {
-            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
-        }
+		if (null === $user) {
+			throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+		}
 
-        return $this->render('@FOSUser/Registration/check_email.html.twig', array(
-            'user' => $user,
-        ));
-    }
+		return $this->render('@FOSUser/Registration/check_email.html.twig', array(
+			'user' => $user,
+		));
+	}
 
-    /**
+	/**
      * Receive the confirmation token from user email provider, login the user.
      *
      * @param Request $request
@@ -131,123 +131,123 @@ class RegistrationController extends BaseController
      *
      * @return Response
      */
-    public function confirmAction(Request $request, $token)
-    {
-        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
-        $userManager = $this->get('fos_user.user_manager');
+	public function confirmAction(Request $request, $token)
+	{
+		/** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+		$userManager = $this->get('fos_user.user_manager');
 
-        $user = $userManager->findUserByConfirmationToken($token);
+		$user = $userManager->findUserByConfirmationToken($token);
 
-        if (null === $user) {
-            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
-        }
+		if (null === $user) {
+			throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
+		}
 
-        /** @var $dispatcher EventDispatcherInterface */
-        $dispatcher = $this->get('event_dispatcher');
+		/** @var $dispatcher EventDispatcherInterface */
+		$dispatcher = $this->get('event_dispatcher');
 
-        $user->setConfirmationToken(null);
-        $user->setEnabled(true);
+		$user->setConfirmationToken(null);
+		$user->setEnabled(true);
 
-        $event = new GetResponseUserEvent($user, $request);
-        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRM, $event);
+		$event = new GetResponseUserEvent($user, $request);
+		$dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRM, $event);
 
-        $userManager->updateUser($user);
+		$userManager->updateUser($user);
 
-        if (null === $response = $event->getResponse()) {
-            $url = $this->generateUrl('fos_user_registration_confirmed');
-            $response = new RedirectResponse($url);
-        }
+		if (null === $response = $event->getResponse()) {
+			$url = $this->generateUrl('fos_user_registration_confirmed');
+			$response = new RedirectResponse($url);
+		}
 
-        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
+		$dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /**
+	/**
      * Tell the user his account is now confirmed.
      */
-    public function confirmedAction()
-    {
-        $user = $this->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
+	public function confirmedAction()
+	{
+		$user = $this->getUser();
+		if (!is_object($user) || !$user instanceof UserInterface) {
+			throw new AccessDeniedException('This user does not have access to this section.');
+		}
 
-        return $this->render('@FOSUser/Registration/confirmed.html.twig', array(
-            'user' => $user,
-            'targetUrl' => $this->getTargetUrlFromSession(),
-        ));
-    }
+		return $this->render('@FOSUser/Registration/confirmed.html.twig', array(
+			'user' => $user,
+			'targetUrl' => $this->getTargetUrlFromSession(),
+		));
+	}
 
-    /**
+	/**
      * @return mixed
      */
-    private function getTargetUrlFromSession()
-    {
-        $key = sprintf('_security.%s.target_path', $this->get('security.token_storage')->getToken()->getProviderKey());
+	private function getTargetUrlFromSession()
+	{
+		$key = sprintf('_security.%s.target_path', $this->get('security.token_storage')->getToken()->getProviderKey());
 
-        if ($this->get('session')->has($key)) {
-            return $this->get('session')->get($key);
-        }
-    }
+		if ($this->get('session')->has($key)) {
+			return $this->get('session')->get($key);
+		}
+	}
 
-    /**
+	/**
     * Builds data to be sent to the mixed login-register template
     * @param  FormView  $registrationForm
     * @param  Request $request
     * @param  string  $div  used to jump to the "#signup"
     * @return array
     */
-    private function buildTemplateData($registrationForm, Request $request, $div)
-    {
-        
-        // This part of code is copied from the SecurityController
-        // It is used to build & send necessary data to the view
-        // since it is a mixed login-register view, so that the user can switch
-        // between both without errors (lacking variables)
-        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
-        $session = $request->getSession();
+	private function buildTemplateData($registrationForm, Request $request, $div)
+	{
 
-        $authErrorKey = Security::AUTHENTICATION_ERROR;
-        $lastUsernameKey = Security::LAST_USERNAME;
+		// This part of code is copied from the SecurityController
+		// It is used to build & send necessary data to the view
+		// since it is a mixed login-register view, so that the user can switch
+		// between both without errors (lacking variables)
+		/** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+		$session = $request->getSession();
 
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has($authErrorKey)) {
-            $error = $request->attributes->get($authErrorKey);
-        } elseif (null !== $session && $session->has($authErrorKey)) {
-            $error = $session->get($authErrorKey);
-            $session->remove($authErrorKey);
-        } else {
-            $error = null;
-        }
+		$authErrorKey = Security::AUTHENTICATION_ERROR;
+		$lastUsernameKey = Security::LAST_USERNAME;
 
-        if (!$error instanceof AuthenticationException) {
-            $error = null; // The value does not come from the security component.
-        }
+		// get the error if any (works with forward and redirect -- see below)
+		if ($request->attributes->has($authErrorKey)) {
+			$error = $request->attributes->get($authErrorKey);
+		} elseif (null !== $session && $session->has($authErrorKey)) {
+			$error = $session->get($authErrorKey);
+			$session->remove($authErrorKey);
+		} else {
+			$error = null;
+		}
 
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+		if (!$error instanceof AuthenticationException) {
+			$error = null; // The value does not come from the security component.
+		}
 
-        $csrfToken = $this->has('security.csrf.token_manager')
-            ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
-            : null;
+		// last username entered by the user
+		$lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
 
-        // Login code ends here
-        
-        // build the entire data to be sent to the view
-        $templateData= array(
-            'last_username' => $lastUsername,
-            'error' => $error,
-            'csrf_token' => $csrfToken,
-            'registration_form'=>$registrationForm,
-        );
+		$csrfToken = $this->has('security.csrf.token_manager')
+			? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+			: null;
 
-        // this div selector will be sent to the view/template
-        // so that we can jump straight to the #signup section if we are in a register context
-        // check the login.html.twig template to see how it works
-        if (!(empty($div))) {
-            $templateData['div']=$div;
-        }
-        return $templateData;
-    }
+		// Login code ends here
+
+		// build the entire data to be sent to the view
+		$templateData= array(
+			'last_username' => $lastUsername,
+			'error' => $error,
+			'csrf_token' => $csrfToken,
+			'registration_form'=>$registrationForm,
+		);
+
+		// this div selector will be sent to the view/template
+		// so that we can jump straight to the #signup section if we are in a register context
+		// check the login.html.twig template to see how it works
+		if (!(empty($div))) {
+			$templateData['div']=$div;
+		}
+		return $templateData;
+	}
 }
